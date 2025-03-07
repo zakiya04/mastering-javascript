13f6344c97f7c297d6701981b17581a9c1be22a6
Zakiya <zakiyamanzoor04@gmail.com>
2025-03-07 12:16:15 +0530

    promise chaining

--- async/index.js
+++ async/index.js
@@ -4,7 +4,14 @@ const cart = ['pants', 'shirt', 'jacket'];
 const promise = createCart(cart);
 
 promise.then(function(orderId){
-  console.log('Promise is working', orderId)
+  console.log('Promise is working', orderId);
+  return orderId
+})
+.then(function(orderId){
+  return  proceedtoPayment(orderId) //returns a promise//
+})
+.then(function(paymentInfo){
+  console.log(paymentInfo)
 })
 //this part handles the rejection part and gives a callback regarding that//
 .catch(function(err){
@@ -17,7 +24,7 @@ function createCart(cart){
     const pr = new Promise(function(resolve,reject){
         
         function validateCart(cart){
-            return false
+            return true
         }
         if(!validateCart(cart)){
             const err = new Error ('cart not valid')
@@ -36,4 +43,9 @@ function createCart(cart){
    // the promise then gets returned to the corresponding variable//
    return pr
 }
- 
+
+function proceedtoPayment(orderId){
+    return new Promise(function(resolve,reject){
+        resolve('Payment Successful!')
+    })
+}
