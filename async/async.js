160c37e4dd5d9517deff8910c8a92e21d4304c3e
Zakiya <zakiyamanzoor04@gmail.com>
2025-03-08 14:14:16 +0530

    returning a promise to async

--- async/async.js
+++ async/async.js
@@ -4,9 +4,17 @@
 async function getData(){
     // this async function always retutns a promise//
     //either you rteun a made promise yourself or it just automatically converts//
-    return "Hello"
+    return pr
 }
 
 const dataPromise = getData(); // this is a promise//
 
 dataPromise.then((res)=> console.log(res)) // this is the value of the promise//
+
+
+//your own promise//
+
+let pr = new Promise ((resolve,reject)=> {
+    resolve('hello')
+})
+// this will return a promise and wont be further wrapped in another promise//
