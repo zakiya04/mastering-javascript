6aff948cb1326ee7290d2aa6a58cc7deca383d34
Zakiya <zakiyamanzoor04@gmail.com>
2025-03-11 19:59:28 +0530

    basic motion of snake

--- projects/snake-game/sanke.js
+++ projects/snake-game/sanke.js
@@ -2,3 +2,14 @@ let board = document.getElementById('board');
 let ctx = board.getContext('2d');
 ctx.fillStyle = 'green';
 ctx.fillRect(0,0, board.width, board.height);
+
+let snake = [{x: 50 ,y:50}];
+let direction = 'right'
+
+function moveSnake(){
+    snake.forEach((position) =>{
+        ctx.fillStyle = 'black';
+        ctx.fillRect(position.x , position.y , 20, 20)
+    })
+}
+moveSnake()
