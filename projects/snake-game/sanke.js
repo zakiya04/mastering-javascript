a96f5d130e44dd0b344761fa5a364d690ecbad60
Zakiya <zakiyamanzoor04@gmail.com>
2025-03-12 20:21:54 +0530

    basic motion of snake

--- projects/snake-game/sanke.js
+++ projects/snake-game/sanke.js
@@ -4,12 +4,25 @@ ctx.fillStyle = 'green';
 ctx.fillRect(0,0, board.width, board.height);
 
 let snake = [{x: 50 ,y:50}];
-let direction = 'right'
+let direction = 'right';
+let speed = 2;
 
-function moveSnake(){
-    snake.forEach((position) =>{
-        ctx.fillStyle = 'black';
-        ctx.fillRect(position.x , position.y , 20, 20)
-    })
+function showSnake(){
+  ctx.fillStyle ='black';
+  ctx.fillRect(snake[0].x,snake[0].y, 30, 30)
 }
-moveSnake()
+function moveSnake(){
+   ctx.clearRect(0,0, board.width, board.height); //clear background//
+   ctx.fillStyle = 'green'; 
+   ctx.fillRect(0,0, board.width, board.height); //fill background//
+   snake.forEach((pos) => {
+    pos.x += speed;
+   })
+   showSnake()
+}
+// motion of the snake//
+function gameLoop(){
+    moveSnake();
+    requestAnimationFrame(gameLoop)
+}
+gameLoop()
