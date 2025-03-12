d2eed750d440ac5c5f2f634372f5594e8e3f4c64
Zakiya <zakiyamanzoor04@gmail.com>
2025-03-12 20:44:36 +0530

    food and snake motion

--- projects/snake-game/sanke.js
+++ projects/snake-game/sanke.js
@@ -4,25 +4,33 @@ ctx.fillStyle = 'green';
 ctx.fillRect(0,0, board.width, board.height);
 
 let snake = [{x: 50 ,y:50}];
+let food = [{x: 220, y: 250}];
 let direction = 'right';
-let speed = 2;
+let speed = 20;
+let score = 0;
 
 function showSnake(){
   ctx.fillStyle ='black';
-  ctx.fillRect(snake[0].x,snake[0].y, 30, 30)
+  ctx.fillRect(snake[0].x,snake[0].y, 28, 28)
 }
 function moveSnake(){
+   let snakeX = snake[0].x;
+   let snakeY = snake[0].y;
    ctx.clearRect(0,0, board.width, board.height); //clear background//
    ctx.fillStyle = 'green'; 
    ctx.fillRect(0,0, board.width, board.height); //fill background//
    snake.forEach((pos) => {
     pos.x += speed;
    })
-   showSnake()
+   showSnake();
+   ctx.fillStyle = 'red';
+   ctx.fillRect(food[0].x, food[0].y, 25, 25)
+   if(snakeX > board.width || snakeY > board.height){
+    clearInterval(game)
+   }
+}
+function collison(){
+
 }
 // motion of the snake//
-function gameLoop(){
-    moveSnake();
-    requestAnimationFrame(gameLoop)
-}
-gameLoop()
+let game = setInterval(moveSnake, 600);
