b2580b7fc73678a1522ff9edf9bd54080931181a
Zakiya <zakiyamanzoor04@gmail.com>
2025-03-13 20:49:09 +0530

    add update functionality

--- projects/snake-game/sanke.js
+++ projects/snake-game/sanke.js
@@ -1,36 +1,52 @@
-let board = document.getElementById('board');
-let ctx = board.getContext('2d');
-ctx.fillStyle = 'green';
-ctx.fillRect(0,0, board.width, board.height);
+let board = document.getElementById("board");
+let ctx = board.getContext("2d");
+ctx.fillStyle = "green";
+ctx.fillRect(0, 0, board.width, board.height);
 
-let snake = [{x: 50 ,y:50}];
-let food = [{x: 220, y: 250}];
-let direction = 'right';
+let snake = [{ x: 50, y: 50 }];
+let food = [{ x: 220, y: 250 }];
+let direction = "right";
 let speed = 20;
 let score = 0;
 
-function showSnake(){
-  ctx.fillStyle ='black';
-  ctx.fillRect(snake[0].x,snake[0].y, 28, 28)
-}
-function moveSnake(){
-   let snakeX = snake[0].x;
-   let snakeY = snake[0].y;
-   ctx.clearRect(0,0, board.width, board.height); //clear background//
-   ctx.fillStyle = 'green'; 
-   ctx.fillRect(0,0, board.width, board.height); //fill background//
-   snake.forEach((pos) => {
-    pos.x += speed;
-   })
-   showSnake();
-   ctx.fillStyle = 'red';
-   ctx.fillRect(food[0].x, food[0].y, 25, 25)
-   if(snakeX > board.width || snakeY > board.height){
-    clearInterval(game)
-   }
-}
-function collison(){
+document.addEventListener("keydown", changeDirection);
 
+function changeDirection(event) {
+  if (event.keyCode == 37 && direction != "RIGHT") direction = "LEFT";
+  else if (event.keyCode == 38 && direction != "DOWN") direction = "UP";
+  else if (event.keyCode == 39 && direction != "LEFT") direction = "RIGHT";
+  else if (event.keyCode == 40 && direction != "UP") direction = "DOWN";
 }
+
+function drawGame() {
+  ctx.clearRect(0, 0, board.width, board.height); //clear background//
+
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, board.width, board.height); //fill background//
+
+  if(direction == 'LEFT') snake[0].x -= speed;
+  if(direction == 'RIGHT') snake[0].x += speed;
+  if(direction == 'UP') snake[0].y -= speed;
+  if(direction == 'DOWN') snake[0].y += speed;
+
+
+  ctx.fillStyle = "black";
+  ctx.fillRect(snake[0].x, snake[0].y, 28, 28);
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(food[0].x, food[0].y, 25, 25);
+}
+
+function collision() {
+  if (
+    snake[0].x >= board.width ||
+    snake[0].y >= board.height ||
+    snake[0].x < 0 ||
+    snake[0].y < 0
+  ) {
+    clearInterval(game);
+  }
+}
+function changeDirection() {}
 // motion of the snake//
-let game = setInterval(moveSnake, 600);
+let game = setInterval(drawGame, 600);
