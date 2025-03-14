d0b5ad1fa4131d827a6777d1ffe9ded1e876265a
Zakiya <zakiyamanzoor04@gmail.com>
2025-03-14 20:36:26 +0530

    random food generator

--- projects/snake-game/sanke.js
+++ projects/snake-game/sanke.js
@@ -5,9 +5,10 @@ ctx.fillRect(0, 0, board.width, board.height);
 
 let snake = [{ x: 50, y: 50 }];
 let food = { x: 220, y: 250 };
-let direction = "right";
+let direction = "RIGHT";
 let speed = 20;
 let score = 0;
+
 
 document.addEventListener("keydown", changeDirection);
 
@@ -17,12 +18,28 @@ function changeDirection(event) {
   else if (event.keyCode == 39 && direction != "LEFT") direction = "RIGHT";
   else if (event.keyCode == 40 && direction != "UP") direction = "DOWN";
 }
+function startGame(){
+  game  = setInterval(drawGame, 200);
+}
+startGame()
 
 function drawGame() {
-  ctx.clearRect(0, 0, board.width, board.height); //clear background//
 
   ctx.fillStyle = "green";
   ctx.fillRect(0, 0, board.width, board.height); //fill background//
+
+ if(
+  snake[0].x < food.x + 20 &&
+  snake[0].x + 20 > food.x &&
+  snake[0].y < food.y + 20 &&
+  snake[0].y + 20 > food.y
+  ){
+    food = {
+      x: Math.floor(Math.random() * (board.width - 20) / 20) * 20,
+      y: Math.floor(Math.random() * (board.height - 20) / 20) * 20
+    };
+    snake.push({})
+  }
 
   if(direction == 'LEFT') snake[0].x -= speed;
   if(direction == 'RIGHT') snake[0].x += speed;
@@ -35,6 +52,7 @@ function drawGame() {
 
   ctx.fillStyle = "red";
   ctx.fillRect(food.x, food.y, 25, 25);
+  collision()
 }
 
 function collision() {
@@ -45,11 +63,15 @@ function collision() {
     snake[0].y < 0
   ) {
     clearInterval(game);
+    alert("Game Over!");
   }
   if (snake[0].x === food.x && snake[0].y === food.y){
     snake.push({x: snake[0].x + speed ,y: snake[0].y})
   }
 }
-function changeDirection() {}
 // motion of the snake//
-let game = setInterval(drawGame, 600);
+
+
+
+
+
