6ca43c151ff4b25f78a80407767b15abaee0db2e
Zakiya <zakiyamanzoor04@gmail.com>
2025-03-16 13:39:37 +0530

    get word list

--- projects/hangman/index.js
+++ projects/hangman/index.js
@@ -1,11 +1,75 @@
 let container = document.getElementById("words");
 
+const wordList = [
+  {
+    category: "Movies",
+    names: [
+      "To Kill a Mockingbird",
+      "Raiders of the Lost Ark",
+      "Casablanca",
+      "High Noon",
+      "The Silence of the Lambs",
+      "Rocky",
+      "Aliens",
+      "Lawrence of Arabia",
+      "The Grapes of Wrath",
+      "Star Wars",
+    ],
+  },
+  {
+    category: "Countries",
+    names: [
+      "China",
+      "India",
+      "United States",
+      "Indonesia",
+      "Brazil",
+      "Pakistan",
+      "Nigeria",
+      "Bangladesh",
+      "Russia",
+      "Japan",
+    ],
+  },
+  {
+    category: "Mahabharata Characters",
+    names: [
+      "Abhimanyu",
+      "Adhiratha",
+      "Amba",
+      "Ambalika",
+      "Ambika",
+      "Arjuna",
+      "Ashwatthama",
+      "Bharata",
+      "Bhima",
+      "Bhisma",
+    ],
+  },
+  {
+    category: "Game of Thrones Characters",
+    names: [
+      "Eddard Stark",
+      "Catelyn Stark",
+      "Robb Stark",
+      "Sansa Stark",
+      "Arya Stark",
+      "Bran Stark",
+      "Rickon Stark",
+      "Jon Snow",
+      "Benjen Stark",
+      "Lyanna Stark",
+    ],
+  },
+];
+
 function getWords() {
   let words = ["abcdefghijklmnopqrstuvxyz"];
   let letters = words[0].split("");
   for (let i = 0; i < letters.length; i++) {
     let btn = document.createElement("button");
     btn.innerText = letters[i];
+    btn.classList.add("btn");
     container.appendChild(btn);
   }
 }
