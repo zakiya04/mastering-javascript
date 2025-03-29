86d7aa8172b053ee171528ab164343ff6ec36fc6
Zakiya <zakiyamanzoor04@gmail.com>
2025-03-29 21:19:10 +0530

    add input

--- projects/kanban-board/board.js
+++ projects/kanban-board/board.js
@@ -1,1 +1,12 @@
-const buttons = document.querySelectorAll("#btn");
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".btn");
+    const container = document.querySelector('.to-do');
+
+    buttons.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            let input = document.createElement('input');
+            input.classList.add('input')
+            container.appendChild(input);
+        });
+    });
+});
