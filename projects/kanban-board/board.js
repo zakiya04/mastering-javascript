deb9cf13ad695f7449bfd6ce78e3fd118b6792fa
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-01 20:28:12 +0530

    drag and drop items

--- projects/kanban-board/board.js
+++ projects/kanban-board/board.js
@@ -1,12 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const buttons = document.querySelectorAll(".btn");
-    const container = document.querySelector('.to-do');
+const buttons = document.querySelectorAll(".btn");
+const container = document.querySelectorAll(".container");
 
-    buttons.forEach((btn) => {
-        btn.addEventListener('click', () => {
-            let input = document.createElement('input');
-            input.classList.add('input')
-            container.appendChild(input);
-        });
-    });
+buttons.forEach((btn,index) => {
+  btn.addEventListener("click", () => {
+    let input = document.createElement("input");
+    input.classList.add("input");
+    input.setAttribute("draggable", "true");
+    input.addEventListener('dragstart',() => input.classList.add('dragging'));
+    input.addEventListener('dragend',() => input.classList.remove('dragging'));
+    if(container[index]){
+      container[index].appendChild(input)
+    }
+  });
 });
+
+container.forEach((cont)=>{
+  cont.addEventListener('dragover',(event) =>{
+    event.preventDefault()
+    const item = document.querySelector('.dragging');
+    cont.appendChild(item)
+  })  
+})
