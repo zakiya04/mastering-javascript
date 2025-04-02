a636a8b755f9a2af34b13ceca5a2133c66d17376
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-02 12:43:19 +0530

    class constructor

--- miscellaneous/objects.js
+++ miscellaneous/objects.js
@@ -14,3 +14,26 @@ function User(name, age, isLoggedIn){
   this.isLoggedIn = isLoggedIn
 }
 const person1 = new User('Dora', 4, true)
+
+//classes in js//
+
+class Person{
+    // this initializes the blueprint and these values are necessary in every object//
+    constructor(name, age, isLoggedIn){
+        this.name = name;
+        this.age = age;
+        this.isLoggedIn = isLoggedIn
+    }
+    //this is secodary ie for some of the objects//
+    getName = function(){
+        console.log(`Hello ${this.name}`)
+    }
+}
+
+const person = new Person('shin', 5, false);
+
+/* 
+POINTS TO NOTE:
+new keyword initializes the constructor()
+classes are typically functions and are sort of syntactic sugar but have more to offer
+*/
