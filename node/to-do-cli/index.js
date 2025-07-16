ecda555d26cdb3117e7f0da3eccd1c39b9d33ba5
Zakiya <zakiyamanzoor04@gmail.com>
2025-07-16 06:31:36 +0530

    add basic cli interface

---
+++ node/to-do-cli/index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+const [,,command,...args]= process.argv;
+const todo = require ('./todo');
+
+switch(command){
+
+    case 'add':
+        todo.addTask(args.join(' '));
+    break;
+
+    case 'list':
+        todo.listTask();
+    break;
+
+    case 'delete':
+        todo.deleteTask(parseInt(args[0]));
+    break;
+
+    case 'done':
+        todo.markDone(parseInt(args[0]));
+    break;
+
+    default:
+        console.log(`
+        Usage:
+        node index.js add "Task Name"
+        node index.js list
+        node index.js done 1
+        node index.js delete 1
+        `)
+}
