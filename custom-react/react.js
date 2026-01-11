bb9a12cd526b9e8e18ef9dd588014e4d8d75bfc9
Zakiya <zakiyamanzoor04@gmail.com>
2026-01-11 15:10:19 +0530

    better js

--- custom-react/react.js
+++ custom-react/react.js
@@ -1,12 +1,23 @@
 
 function createElement(element, container){
+    /*
     const domElement = document.createElement(element.type)
     domElement.innerHTML = element.children;
     domElement.setAttribute('href',element.props.href);
     domElement.setAttribute('target',element.props.target);
 
     container.appendChild(domElement)
-}
+    */
+
+    const domElement = document.createElement(element.type)
+    domElement.innerHTML = element.children;
+    for (const prop in reactElement.props) {
+        if (prop === "children") continue
+        domElement.setAttribute(prop, element.props[prop])
+    };
+
+    container.appendChild(domElement)
+}  
 
 const reactElement = {
     type: "a",
